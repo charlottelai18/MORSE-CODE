3d9282da368222cff9b2536b5737dfd130461872
charlottelai18 <charlottel2015@gmail.com>
2025-12-11 13:17:13 +1100

    writing logic for morse translator

---
+++ script.js
@@ -0,0 +1,59 @@
+export const ENGLISH_TO_MORSE = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",   
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  " ": "/",    
+};
+
+export const MORSE_TO_ENGLISH = Object.fromEntries(
+  Object.entries(ENGLISH_TO_MORSE).map(([letter, morse]) => [morse, letter])
+);
+
+export const ENGLISH_TO_MORSE_TRANSLATE = (text) => {
+    if (typeof text !== 'string') {
+        throw new Error('Input must be a string');
+    }
+
+    return text.toUpperCase().split('').map(char => {
+        if (!(char in ENGLISH_TO_MORSE)) {
+            throw new Error(`Character "${char}" cannot be translated to Morse code`);
+        }
+        return ENGLISH_TO_MORSE[char];
+    }).join(' ');
+}
+
+export const MORSE_TO_ENGLISH_TRANSLATE = (text) => {
+    if (typeof text !== 'string') {
+        throw new Error('Input must be a string');
+    }
+
+    return text.split(' ').map(code => {
+        if (!(code in MORSE_TO_ENGLISH)) {
+            throw new Error(`Morse code "${code}" cannot be translated to English`);
+        }
+        return MORSE_TO_ENGLISH[code];
+    }).join('');
+}
